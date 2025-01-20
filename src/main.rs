13d099535f31e1c0cbce84d3c1e177e6a3f1e150
yontuh <jontesteiner@gmail.com>
2025-01-19 20:38:06 -0800

    Attempting to parse the real gjson data

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,15 @@
+// use std::env;
+use std::fs;
+
+use bevy::log::tracing_subscriber::fmt::writer::OptionalWriter;
 use bevy::prelude::*;
 use bevy::{
     color::palettes::css::{BLUE, GREEN, RED},
-    prelude::*,
+    // prelude::*,
 };
 use bevy_2d_line::LineRenderingPlugin;
 
+use geojson::FeatureCollection;
 use geojson::{Feature, GeoJson, Geometry, Value};
 // use std::convert::TryFrom;
 use bevy_2d_line::Line;
@@ -20,50 +25,53 @@ fn main() {
 fn setup(mut commands: Commands) {
     commands.spawn(Camera2d::default());
 
-    let geojson_str = r#"
-{
-  "type": "Feature",
-  "properties": { "food": "donuts" },
-  "geometry": {
-    "type": "MultiPoint",
-    "coordinates": [
-        [ -101.2836, 34.0956 ],
-        [ -1.2836, 3.0956 ],
-        [ 11.2836, -34.0956 ]
-
-      ]
-  }
-  
-}
-"#;
-
-    let geojson: GeoJson = geojson_str.parse::<GeoJson>().unwrap();
-    let feature: Feature = Feature::try_from(geojson).unwrap();
-
-    // read property data
-    assert_eq!("donuts", feature.property("food").unwrap());
+    let geojson: GeoJson = read_file("assets/germany-detailed-boundary_917.geojson")
+        .parse::<GeoJson>()
+        .unwrap();
+    let feature_collection: FeatureCollection = FeatureCollection::try_from(geojson).unwrap();
 
     // read geometry data
-    let geometry: Geometry = feature.geometry.unwrap();
-    if let Value::MultiPoint(coords) = geometry.value {
-        let coords_1d: Vec<f64> = coords.into_iter().flatten().collect();
+    let features_vec: Vec<Feature> = feature_collection.features;
+
+    for feature in features_vec {
+        let geometry: Geometry = feature.geometry.unwrap();
+        // if let Value::MultiPoint(coords) = geometry.value {
+
+        let geometry_value = geometry.value;
+
+        println!("{}", &geometry_value);
+
+        let coords = match geometry_value {
+            Value::MultiPoint(multipoint) => Some(multipoint),
+            _ => None,
+        };
+
+        dbg!(&coords);
+        // let coords_1d: Vec<f64> = coords.into_iter().flatten().collect();
 
         // Convert the 1D vector into a vector of Vec2 instances
-        let vec2_coords: Vec<Vec2> = coords_1d
-            .chunks(2)
-            .map(|chunk| Vec2::new(chunk[0] as f32, chunk[1] as f32))
-            .collect();
+        // let vec2_coords: Vec<Vec2> = coords_1d
+        // .chunks(2)
+        // .map(|chunk| Vec2::new(chunk[0] as f32, chunk[1] as f32))
+        // .collect();
 
-        let points = vec2_coords.clone();
+        // let points = vec2_coords.clone();
 
-        println!("{:?}\n", vec2_coords);
+        // println!("{:?}\n", vec2_coords);
 
-        let colors = vec![RED.into(), GREEN.into(), BLUE.into()];
+        // let colors = vec![RED.into(), GREEN.into(), BLUE.into()];
 
-        commands.spawn(Line {
-            points,
-            colors,
-            thickness: 5.0,
-        });
+        // commands.spawn(Line {
+        //     points,
+        //     colors,
+        //     thickness: 50000000000000000000000000000.0,
+        // });
+        // }
     }
+}
+
+fn read_file(path: &str) -> String {
+    let contents = fs::read_to_string(path).expect("Should have been able to read the file");
+
+    contents
 }
