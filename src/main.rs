7cc90af182b6e4015e7bd577a9856bdface23c09
yontuh <jontesteiner@gmail.com>
2025-01-21 12:40:19 -0800

    Render a country using geojson!

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 // use std::env;
 use std::fs;
 
+use bevy_polyline::prelude::*;
+
 use bevy::log::tracing_subscriber::fmt::writer::OptionalWriter;
 use bevy::prelude::*;
 use bevy::{
@@ -9,65 +11,76 @@ use bevy::{
 };
 use bevy_2d_line::LineRenderingPlugin;
 
-use geojson::FeatureCollection;
-use geojson::{Feature, GeoJson, Geometry, Value};
+use geojson::*;
 // use std::convert::TryFrom;
 use bevy_2d_line::Line;
+
+use serde_json::from_str;
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
+        .add_plugins(PolylinePlugin)
         .add_plugins(LineRenderingPlugin)
         .add_systems(Startup, setup)
         .run();
 }
 
-fn setup(mut commands: Commands) {
-    commands.spawn(Camera2d::default());
+fn setup(
+    mut commands: Commands,
+    mut polyline_materials: ResMut<Assets<PolylineMaterial>>,
+    mut polylines: ResMut<Assets<Polyline>>,
+) {
+    let geojson_data = read_file("assets/germany-detailed-boundary_917.geojson");
 
-    let geojson: GeoJson = read_file("assets/germany-detailed-boundary_917.geojson")
-        .parse::<GeoJson>()
-        .unwrap();
-    let feature_collection: FeatureCollection = FeatureCollection::try_from(geojson).unwrap();
+    let feature_collection: FeatureCollection = from_str(geojson_data.as_str()).unwrap();
+    let mut coordinates: Vec<Vec3> = Vec::new();
 
-    // read geometry data
-    let features_vec: Vec<Feature> = feature_collection.features;
-
-    for feature in features_vec {
-        let geometry: Geometry = feature.geometry.unwrap();
-        // if let Value::MultiPoint(coords) = geometry.value {
-
-        let geometry_value = geometry.value;
-
-        println!("{}", &geometry_value);
-
-        let coords = match geometry_value {
-            Value::MultiPoint(multipoint) => Some(multipoint),
-            _ => None,
-        };
-
-        dbg!(&coords);
-        // let coords_1d: Vec<f64> = coords.into_iter().flatten().collect();
-
-        // Convert the 1D vector into a vector of Vec2 instances
-        // let vec2_coords: Vec<Vec2> = coords_1d
-        // .chunks(2)
-        // .map(|chunk| Vec2::new(chunk[0] as f32, chunk[1] as f32))
-        // .collect();
-
-        // let points = vec2_coords.clone();
-
-        // println!("{:?}\n", vec2_coords);
-
-        // let colors = vec![RED.into(), GREEN.into(), BLUE.into()];
-
-        // commands.spawn(Line {
-        //     points,
-        //     colors,
-        //     thickness: 50000000000000000000000000000.0,
-        // });
-        // }
+    for feature in feature_collection.features {
+        if let Some(geometry) = feature.geometry {
+            match geometry.value {
+                Value::MultiPolygon(multipolygon) => {
+                    for polygon in multipolygon {
+                        for ring in polygon {
+                            for coordinate in ring {
+                                coordinates.push(Vec3::new(
+                                    coordinate[0] as f32,
+                                    coordinate[1] as f32,
+                                    0.0,
+                                ));
+                            }
+                        }
+                    }
+                }
+                _ => {}
+            }
+        }
     }
+
+    println!("{:?}", &coordinates);
+
+    let points = coordinates.clone();
+
+    commands.spawn(PolylineBundle {
+        polyline: PolylineHandle(polylines.add(Polyline { vertices: points })),
+        material: PolylineMaterialHandle(polyline_materials.add(PolylineMaterial {
+            width: 100.0,
+            color: RED.into(),
+            perspective: true,
+            ..default()
+        })),
+        ..default()
+    });
+
+    commands.spawn((
+        Camera3d::default(),
+        Camera {
+            hdr: true,
+            ..default()
+        },
+        Msaa::Sample4,
+        Transform::from_xyz(11.0, 51.0, 10.0),
+    ));
 }
 
 fn read_file(path: &str) -> String {
